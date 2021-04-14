d10996987c5ef71837c8c4f614e762a3dca1ff9c
Carlosgamer55 <82543400+Carlosgamer55@users.noreply.github.com>
2021-04-14 16:57:54 -0600

    Update welcomtext.js

--- welcomtext.js
+++ welcomtext.js
@@ -1,9 +1,9 @@
 const welcome = (number, groupname) => {
-    return `Hola @${number}. Bienvenido al grupo ${groupname} lee las reglas para no tener mal entendidos`
+    return `Hola @${number}. Bienvenido al grupo ${groupname} presentate con foto o te viola tu tio`
 }
 exports.welcome = welcome
 
 const bye = (number) => {
-    return `Adios joto @${number}. 👋`
+    return `Adios perra, te espero en 4 @${number}. 👋`
 }
 exports.bye = bye
